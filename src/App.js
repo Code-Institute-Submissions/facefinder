1da169b3ece938825c57b53adb29257b953bb49c
Clive <clivenoonan93@gmail.com>
2018-09-02 03:15:40 +0100

    Fixed indention

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class App extends Component {
 
     onButtonSubmit = () => {
         this.setState({imageUrl: this.state.input});
-        fetch(' https://face-finder-backend-api.herokuapp.com/imageurl', {
+        fetch('https://face-finder-backend-api.herokuapp.com/imageurl', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
@@ -92,7 +92,7 @@ class App extends Component {
         .then(response => response.json())
         .then(response => {
           if (response) {
-            fetch(' https://face-finder-backend-api.herokuapp.com/image', {
+            fetch('https://face-finder-backend-api.herokuapp.com/image', {
                method: 'put',
                headers: { 'Content-Type': 'application/json' },
                body: JSON.stringify({
