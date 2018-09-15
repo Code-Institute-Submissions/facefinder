5dbd63e5305d11df8cc271279a6f9c498598836f
Clive <clivenoonan93@gmail.com>
2018-09-15 01:42:13 +0100

    fixed my project absolutly delgihted

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Tilt from 'react-tilt';
-import brain from './cyborg.png';
+import cyborg from './cyborg.png';
 import './Logo.css';
 
 const Logo = () => {
@@ -8,7 +8,7 @@ const Logo = () => {
     <div className='ma4 mt0'>
       <Tilt className="Tilt br2 shadow-2" options={{ max : 65 }} style={{ height: 200, width: 200 }} >
         <div className="Tilt-inner pa3">
-          <img style={{paddingTop: '10px'}} alt='logo' src={brain}/>
+          <img style={{paddingTop: '10px'}} alt='logo' src={cyborg}/>
         </div>
       </Tilt>
     </div>
